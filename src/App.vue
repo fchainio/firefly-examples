084b6ae2c0a902eb1c1dfea1d12a293cc56ce1e8
imloama <imloama@gmail.com>
2018-08-22 12:37:03 +0800

    use new api

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import VConsole from 'vconsole'
+const vconsole = new VConsole();
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/CodeSection.vue
@@ -0,0 +1,58 @@
+//代码与功能
+<template>
+  <div class="codesection">
+    <div class="title">
+      <slot name="title"></slot>
+    </div>
+    <div class="code">
+      <slot name="code"></slot>
+    </div>
+    <div class="actions">
+      <slot name="actions"></slot>
+    </div>
+    <div class="result">
+      执行结果：<br/><br/>
+      <slot name="result"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.codesection{
+  margin: 10px 10px;
+  /* padding: 8px 8px; */
+  background: #e3e3e3;
+  border:1px solid #e3e3e3;
+  border-radius: 10px;
+  font-size: 14px;
+  text-align: left;
+}
+.title{
+  padding: 8px 8px;
+  font-size: 16px;
+  font-weight: 500;
+  background: #999999;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+.code{
+  overflow: auto;
+  padding: 8px 8px;
+  
+}
+.result{
+  padding: 8px 8px;
+  border-top: 1px solid #999999;
+  overflow: auto;
+}
+.actions{
+  padding: 8px 8px;
+  border-top: 1px solid #999999;
+}
+</style>
