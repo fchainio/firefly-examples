c5df08593f67c2227f6653a3bbd222a673750993
Mickey0923 <474792066@qq.com>
2019-05-06 10:11:15 +0800

    样式修改

--- dapp/vue_demo/src/App.vue
+++ dapp/vue_demo/src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="firefly logo" src="./assets/logo.png">
+      <img class='logo' src="@/assets/logo.png" alt="">
+        <h1 class='index_t_h1'>FireFly Wallet DApp Demo</h1>
+        <p class='index_t_p'>FireFly Wallet DApp DemoFireFly Wallet DApp Demo FireFly Wallet DApp Demo</p>
     <HelloWorld/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        padding: 0.4167rem;
+    }
+    
+    .logo {
+        width: 1.0648rem;
+        height: 1.0648rem;
+        margin: 0.5556rem auto;
+    }
+    
+    .index_t_h1 {
+        font-size: 0.4444rem;
+        color: #213a5a;
+        margin-bottom: 0.3704rem;
+    }
+    
+    .index_t_p {
+        font-size: 0.3333rem;
+        color: #717f90;
+        line-height: 1.5;
+        margin-bottom: 0.7407rem;
+    }
 </style>
--- dapp/vue_demo/src/main.js
+++ dapp/vue_demo/src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'lib-flexible';
 import Prism from 'prismjs'
 // Prism.highlightAll()
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
